4aaa52bda7dd1461191369908b18fa2cbe466684
Joel Cretan <joel.cretan@mandiant.com>
2023-04-17 17:34:53 -0400

    lint

--- packages/x64dbg.dbgchild.vm/tools/chocolateyuninstall.ps1
+++ packages/x64dbg.dbgchild.vm/tools/chocolateyuninstall.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'Continue'
 
 $toolDir = Join-Path ${Env:RAW_TOOLS_DIR} 'x64dbg\release'
 foreach ($file in $("NewProcessWatcher.exe", "readme_dbgchild.txt", "x64_post.unicode.txt", "x64_pre.unicode.txt", "x86_post.unicode.txt", "x86_pre.unicode.txt", "dbgchildlogs")) {
-    Remove-Item "${toolDir}\${file}" -Recurse -Force 
+    Remove-Item "${toolDir}\${file}" -Recurse -Force
 }
 
 $archFiles = @("CreateProcessPatch.exe", "DbgChildHookDLL.dll", "NTDLLEntryPatch.exe", "CPIDS")
